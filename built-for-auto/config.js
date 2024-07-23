9b6f81f289ebef4acd00e700a0c420c76508a546
rnzee <97718336+rnzee@users.noreply.github.com>
2024-07-23 16:54:23 -0400

    Updates

--- built-for-auto/config.js
+++ built-for-auto/config.js
@@ -1,13 +1,13 @@
 let titleDiv = 
-'<img src="../images/MAS-Full-BY.png" alt="MAS Logo" height=45px width=auto><p></p>'
+'<img src="../images/MAS-Full-BY.png" alt="MAS Logo" height=75px width=auto><br><br>'
 +'<h1>Greener Corridors for a More Resilient City</h1>';
 
 let subtitleDiv = 
 '<h2>Issues & Opportunities</h2><p></p>';
 
 let bylineDiv = 
-'<p class="p-header"><a href="https://www.mas.org/initiatives/greener-corridors/" target="_blank">Greener Corridors</a> examines New York City’s largest arterial thoroughfares as an opportunity to create more livable, efficient, and green public spaces. These “<a href="https://www.planetizen.com/news/2024/05/129032-stroads-worst-both-worlds" target="_blank">stroads</a>” are frequently criticized for road designs that prioritize moving large volumes of through-traffic at the expense of pedestrian and cyclist safety, public health, and walkable street environments that foster a sense of place and belonging. This storymap series takes a deeper look at the spatial relationship between these thoroughfares and mobility, sustainability, and land use issues to better understand where and how these corridors might be improved.</p><br>'
-+'<div class="row">'
+'<p class="p-header"><a href="https://www.mas.org/initiatives/greener-corridors/" target="_blank">Greener Corridors</a> examines New York City’s largest arterial thoroughfares as an opportunity to create more livable, efficient, and green public spaces. As a mix of urban street (where people live and shop) and road (designed to quickly move large volumes of through-traffic), such corridors are sometimes referred to as “<a href="https://www.planetizen.com/news/2024/05/129032-stroads-worst-both-worlds" target="_blank">stroads</a>” in urban planning circles. This street-road mix is often incompatible, and stroads are frequently criticized for being inefficient, unappealing, dangerous, and too focused on through-movement at the expense of walkable environments that foster a sense of place. This storymap series takes a deeper look at the spatial relationship between these thoroughfares and mobility, sustainability, and land use issues to better understand where and how these corridors might be improved.</p><br>'
++'<div class="row">' 
   +'<div class="column"><h1><mark>PART 1</mark></h1></div>'
   +'<div class="column"></div>'
   +'<div class="column"></div></div><br><hr>'
@@ -96,7 +96,7 @@ var config = {
       alignment: "centered",
       hidden: false,
       chapterDiv: 
-      '<p class="p-center">That includes more than 120,000 trucks that enter and exit the city daily, primarily along the city’s arterials because most are designated as truck routes and connect to distribution centers and industrial areas.</p>'
+      '<p class="p-center">That includes more than 120,000 trucks that enter and exit the city daily, primarily along the city’s arterials because most are designated as truck routes and connect to distribution centers and industrial areas. With the demand for goods only growing, freight in its many forms—such as package deliveries, construction services, and sanitation—will continue to coexist with other users of the street.</p>'
       +'<figure>'
         +'<img src="./images/atlantic-ave-6.jpg" alt="Two trucks waiting at a stoplight" width=100%>'
         +'<figcaption>“[D]own on the ten-lane surface road beneath the [Gowanus] parkway, rumbled (from before dawn until after dark after the opening of the Brooklyn-Battery Tunnel flooded the area with freight traffic) regiments, brigades, divisions of huge tractor-trailer trucks, engines gunning and backfiring, horns blasting, brakes screeching...” &mdash;From Robert Caro’s <em>The Power Broker</em> on the impacts of Moses-era highway development on Brooklyn’s Third Avenue, which evokes similar impressions today. Photo: Kade Van Meeteran.</figcaption></figure>',
@@ -184,7 +184,7 @@ var config = {
       chapterDiv: 
       '<p>Unsurprisingly, residents along corridors in outlying areas (such as North Conduit Avenue, Springfield Boulevard, and Utopia Parkway in eastern Queens and Baychester Avenue in the northeast Bronx) have higher vehicle ownership rates.</p><br>'
       +'<dl><dt><strong>Legend</strong></dt>'
-        +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#7882a7" /></svg>41% to 58%</dd>'
+        +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#7882a7" /></svg>41% to 58% of households in census tract own vehicle(s)</dd>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#556197" /></svg>59% to 75%</dd>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#2d4387" /></svg>More than 75%</dd>'
         +'<dd><svg width="17" height="12"><line x1="2" y1="0" x2="10" y2="10" style="stroke:#f35d17;stroke-width:2" /></svg>Stroads in areas with higher vehicle ownership</dd></dl>',
@@ -267,7 +267,7 @@ var config = {
       chapterDiv: 
       '<p>While public transportation usage is expectedly high among commuters in denser areas, it is also utilized in more far-flung neighborhoods of New York City with greater vehicle ownership. Among commuting residents who live within walking distance of stroads in EJ areas, about 62% walk, bike, or take public transportation to work, about three percent higher than the citywide average and far higher than any other U.S. city (in Boston, the U.S. city with the next highest average, about 40% of commuting residents walk, bike, or take public transportation to work).</p><br>'
       +'<dl><dt><strong>Legend</strong></dt>'
-        +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#b5caed" /></svg>Less than 20% of population in census tract takes public transportation to work</dd>'
+        +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#b5caed" /></svg>Less than 20% of population in census tract walks, bikes, or takes public transit to work</dd>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#a4a7d8" /></svg>20% to 47%</dd>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#9284c2" /></svg>48% to 58%</dd>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#7f62ad" /></svg>59% to 67%</dd>'
@@ -383,7 +383,7 @@ var config = {
       alignment: 'full',
       hidden: false,
       chapterDiv:
-      '<p>Arterials lined with alternate side parking forces taxis and delivery drivers to double park in spaces intended for vehicle movement or pedestrian use in order to complete pickups and deliveries.</p>'
+      '<p>Stroads lined with poorly managed curb space leads taxis and delivery drivers to double park in lanes intended for vehicle movement or pedestrian use in order to complete pickups and deliveries.</p>'
       +'<figure>'
         +'<img src="./images/google-street-view/streetview-northern-blvd.png" alt="Truck parked on chicane" width=100%>'
         +'<figcaption>Northern Boulevard, Queens. Image: Google Street View, 2022.</figcaption></figure>',
@@ -404,7 +404,7 @@ var config = {
       alignment: 'full',
       hidden: false,
       chapterDiv: 
-      '<p>The rightmost travel lane along numerous corridors is rendered virtually useless as a result.</p>'
+      '<p>The rightmost travel lane along numerous corridors is rendered virtually useless as a result, both demonstrating the inefficiencies of the system and raising questions about the appropriate number and types of vehicle lanes.</p>'
       +'<figure>'
         +'<img src="./images/google-street-view/streetview-boston-rd.png" alt="Cars on travel lane" width=100%>'
         +'<figcaption>Boston Road, The Bronx. Image: Google Street View, 2022.</figcaption></figure>',
@@ -426,6 +426,7 @@ var config = {
       hidden: false,
       chapterDiv: 
       '<p>Where they have been installed, clearly delineated bus and bike lanes have improved multi-modal traffic flow, but they too are regularly blocked and unusable when appropriate lane use is not enforced.</p>'
+      +'<p>One widely accepted strategy for managing the safety and flow of right-of-way users is a <a href="https://smartgrowthamerica.org/what-are-complete-streets/">complete streets policy</a>. However, a 2019 report by researchers at the City College of New York and the Department of Transportation (DOT) observed that inclusion of freight challenges in complete streets models has been limited, even in New York City. By incorporating freight needs into such models, conflicts like large vehicles encroaching on sidewalks and blind spots that endanger cyclists may be tackled more effectively.</p>'
       +'<figure>'
         +'<img src="./images/google-street-view/streetview-utica-ave.png" alt="Truck on bus lane ahead of a bus circumventing on the travel lane" width=100%>'
         +'<figcaption>Utica Avenue, Brooklyn. Image: Google Street View, 2022.</figcaption></figure>',
@@ -450,7 +451,7 @@ var config = {
       alignment: 'centered',
       hidden: false,
       chapterDiv: 
-      '<p class="p-center">And even on the sidewalk, pedestrians must routinely meander around large vehicles, automobile-related equipment, and daunting access roads to suburban-style shopping plazas, drive-throughs, and other neighboring properties that prevent safe passage. It leads to neighborhood spaces that people seek to avoid altogether and is at odds with how the City views many of these corridors in terms of function and experience.</p>',
+      '<p class="p-center">Even on the sidewalk, pedestrians must routinely meander around large vehicles, automobile-related equipment, and daunting access roads to suburban-style shopping plazas, drive-throughs, and other neighboring properties that prevent safe passage. It leads to neighborhood spaces that people seek to avoid altogether and is at odds with how the City views many of these corridors in terms of function and experience.</p>',
       location: {
         center: [-74, 40.73800],
         zoom: 10.3,
@@ -468,7 +469,7 @@ var config = {
       alignment: 'left',
       hidden: false,
       chapterDiv: 
-      '<p>For example, the Department of Transportation’s (DOT) <a href="https://www.nyc.gov/html/dot/html/pedestrians/pedestrian-mobility.shtml" target="_blank">Pedestrian Mobility Plan</a> categorizes every street segment citywide into one of five types that reflect different levels of pedestrian need on the city’s sidewalks. According to the plan, 42% of our study’s stroads are at least “Neighborhood Corridors.” That is, they have consistent pedestrian destinations that should ideally accommodate small groups of people passing each other on foot. The reality can feel very different.</p><br>'
+      '<p>For example, the Department of Transportation’s (DOT) <a href="https://www.nyc.gov/html/dot/html/pedestrians/pedestrian-mobility.shtml" target="_blank">Pedestrian Mobility Plan</a> categorizes every street segment citywide into one of five types (listed below) that reflect different levels of pedestrian need on the city’s sidewalks. According to the plan, 42% of our study’s stroads are at least “Neighborhood Corridors.” That is, they have consistent pedestrian destinations that should ideally accommodate small groups of people passing each other on foot. The reality can feel very different.</p><br>'
       + '<dl><dt><strong>Legend</strong></dt>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#432371" /></svg><strong>Global Corridors</strong> have large crowds of people and high concentration of global destinations</dd>'
         +'<dd><svg width="17" height="12"><rect width="10" height="10" x="1" y="1" style="fill:#754276" /></svg><strong>Regional Corridors</strong> have crowds of people and attractions that draw people from the region</dd>'
@@ -565,7 +566,7 @@ var config = {
       alignment: 'centered',
       hidden: false,
       chapterDiv: '<p class="p-center">Of course, even with available public transit, cities as dense as New York still need streets that can accommodate heavy amounts of vehicle traffic and their array of destinations. It is also important to acknowledge that the City is taking steps to address a number of the issues that plague these thoroughfares.</p>'
-      +'<p class="p-center">In addition to DOT’s Priority Corridors,  many stroads have been designated as <a href="https://www.nyc.gov/html/visionzero/pages/initiatives/street-design.shtml" target="_blank">Vision Zero Arterial Slow Zones</a>, and <a href="https://www.nyc.gov/content/visionzero/pages/engineering" target="_blank">Great Streets</a> program investments along stroads like Queens Boulevard have made them significantly safer and more pleasant. The City has also piloted busways and curb management programs, is redesigning its truck route network, passed a City of Yes text amendment to reduce pedestrian-automobile conflicts, and has completed a number of other projects and studies that will be featured in a future <em>Greener Corridors</em> post.</p>',
+      +'<p class="p-center">In addition to DOT’s Priority Corridors, many stroads have been designated as <a href="https://www.nyc.gov/html/visionzero/pages/initiatives/street-design.shtml" target="_blank">Vision Zero Arterial Slow Zones</a>, and <a href="https://www.nyc.gov/content/visionzero/pages/engineering" target="_blank">Great Streets</a> program investments along stroads like Queens Boulevard have made them significantly safer and more pleasant. Other City-led initiatives include new busway and curb management pilot programs, a provision in the City of Yes for Economic Opportunity text amendment to reduce pedestrian-automobile conflicts, and ongoing efforts to overhaul its truck route network and last-mile delivery system.</p>',
       location: {
         center: [-74, 40.73800],
         zoom: 10.3,
@@ -588,7 +589,7 @@ var config = {
     { id: 'auto-21',
       alignment: 'centered',
       hidden: false,
-      chapterDiv: '<p class="p-center">Yet even with ongoing and proposed improvements, these corridors are not being optimized to their full potential, with mobility being just one opportunity area. The need to leverage them is all the more urgent given the climate crisis and transportation’s role as the largest contributor to greenhouse gas emissions in the country. For those who have the choice of how to travel, dangerous, unpleasant, and car-oriented street conditions reinforce a vicious cycle where driving becomes the preferred option. In Part 2 of this storymap, we will explore issues of heat and flooding and  opportunities to make these corridors more climate resilient.</p>'
+      chapterDiv: '<p class="p-center">Yet even with ongoing and proposed improvements, these corridors are not being optimized to their full potential, with mobility being just one focus area. There is an opportunity to make stroads significantly greener in terms of trees and vegetation and to encourage more sustainable development. The need to leverage them is all the more urgent given the climate crisis and transportation’s role as the largest contributor to greenhouse gas emissions in the country. For those who have the choice of how to travel, dangerous, unpleasant, and car-oriented street conditions reinforce a vicious cycle where driving becomes the preferred option. In Part 2 of this storymap, we will explore issues of heat and flooding and opportunities to make these corridors more climate resilient.</p>'
       +'<figure>'
         +'<img src="./images/boston-rd-1.jpg" alt="Scooters left astride a sidewalk in front of an automotive business" width=100%>'
         +'<figcaption>The one-year East Bronx Shared E-Scooter Pilot expanded mobility options to East Bronx residents beginning in the summer 2021. Since then, more than 3.4 million trips have been taken. Boston Road was one of several thoroughfares that reported high scooter traffic volumes, suggesting the need for safer, designated lanes for micromobility users.</figcaption></figure>',
